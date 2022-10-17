378d2cda592db7519b51b24ac7d6a44f9d743365
Vlad Bolkunov <gloryson@mail.ru>
2022-10-17 10:33:16 +0300

    feat: add store files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AppProvider from "./store/AppProvider";
 
 
 
@@ -7,6 +8,8 @@ export default function App () {
 
 
   return (
-    <></>
+    <AppProvider>
+      
+    </AppProvider>
   );
 }
